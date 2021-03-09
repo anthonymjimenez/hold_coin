95ca2602707f991faec65beb7ea7d6630bb58b79
anthonymjimenez <anthonyjim96@gmail.com>
2021-03-09 18:45:18 -0500

    added amplify API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import Amplify from 'aws-amplify';
+import Amplify, {API} from 'aws-amplify';
 import config from './aws-exports';
 import {withAuthenticator} from '@aws-amplify/ui-react';
 
 Amplify.configure(config);
 
 function App() {
+  React.useEffect(() => {
+    API.get("cryptoapi", "/crypto/symbol").then(console.log)
+  })
   return (
     <div className="App">
       <header className="App-header">
